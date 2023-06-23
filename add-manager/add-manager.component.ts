aeee09c05e0cbb2cca5666abffe05002562eec5a
Yasmine ahmed <120103072+YasmineAhmed99@users.noreply.github.com>
2023-06-23 20:16:48 +0300

    Add files via upload

---
+++ add-manager/add-manager.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import { AddmanagerService } from '../Services/addmanger.service';
+
+import { Manager } from '../_models/Manger';
+@Component({
+  selector: 'app-add-manager',
+  templateUrl: './add-manager.component.html',
+  styleUrls: ['./add-manager.component.css']
+})
+export class AddManagerComponent {
+
+  nmanager:Manager=new Manager("","","","","");
+  constructor(public manSer:AddmanagerService,private router: Router){}
+  Add(){
+      this.manSer.add(this.nmanager).subscribe(a=>{
+        console.log(a);
+      
+      },er=>{
+        console.log(er)
+      })
+  }
+}
+
